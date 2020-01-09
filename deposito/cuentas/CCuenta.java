bf165dd501b784138e5bae782350b20970aa3ea9
PheloPC <PheloPC@PheloPC-PC>
2020-01-09 21:08:27 +0100

    Comentarios agregados a CCuenta.java

--- deposito/cuentas/CCuenta.java
+++ deposito/cuentas/CCuenta.java
@@ -1,4 +1,15 @@
 package cuentas;
+/**
+ * Esta clase define las operaciones que se pueden realizar con el saldo, aumentándolo, disminuyéndolo o manteniéndolo
+ * @author Manuel Acevedo Sánchez
+ * @version 2.1
+ * @category tareas
+ * @see <a href=https://github.com/manuelacevedosanchez/repository1>Repositorio en GitHub</a>
+ */
+/**
+ * @author PheloPC
+ *
+ */
 public class CCuenta {
 
 
@@ -11,6 +22,12 @@ public class CCuenta {
     {
     }
 
+    /**
+     * @param nom nombre del titular
+     * @param cue cuenta sobre la que se opera
+     * @param sal saldo en cada momento
+     * @param tipo tipo de interés aplicable a la operación
+     */
     public CCuenta(String nom, String cue, double sal, double tipo)
     {
         setNombre(nom);
@@ -18,11 +35,18 @@ public class CCuenta {
         setSaldo(sal);
     }
 
+    /**
+     * @return devuelve el saldo actual
+     */
     public double estado()
     {
         return getSaldo();
     }
 
+    /**
+     * @param cantidad monto de importe para la operación
+     * @throws Exception mensaje que se muestra en pantalla para valores negativos no admisibles
+     */
     public void ingresar(double cantidad) throws Exception
     {
         if (cantidad<0)
@@ -30,6 +54,10 @@ public class CCuenta {
         setSaldo(getSaldo() + cantidad);
     }
 
+    /**
+     * @param cantidad monto de importe para la operación
+     * @throws Exception mensaje que se muestra en pantalla para valores negativos no admisibles o si la cantidad supera el saldo disponible
+     */
     public void retirar(double cantidad) throws Exception
     {
         if (cantidad <= 0)
@@ -39,6 +67,10 @@ public class CCuenta {
         setSaldo(getSaldo() - cantidad);
     }
 
+	/**
+	 * Getters y Setters para obtener y retornar valores asociados a los parámetros
+	 */    
+    
 	private String getNombre() {
 		return nombre;
 	}
